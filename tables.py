ac030abde704d89258731a8a5031b67aded1de1c
Jonathan Moyett <someguy590@gmail.com>
2020-10-07 20:46:59 -0500

    add roll tables

---
+++ tables.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+def out():
+    n = randint(0, 9)
+
+
+def defense():
+    n = randint(1, 12)
+
+
+def hit():
+    n = randint(1, 20)
+
+
+def crit_hit():
+    n = randint(1, 20)
