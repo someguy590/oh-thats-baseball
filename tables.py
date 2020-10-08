2a45c905a871a5f1a849b10418cb0879ab1199df
Jonathan Moyett <someguy590@gmail.com>
2020-10-08 05:20:43 -0500

    fill out defense, hit, and crit hit tables

--- tables.py
+++ tables.py
@@ -1,4 +1,6 @@
 from random import randint
+from deadball_enums import SwingResultType, DefenseResult
+from swing_result import SwingResult
 
 
 def out():
@@ -23,11 +25,61 @@ def out():
 
 def defense():
     n = randint(1, 12)
+    if n <= 2:
+        return DefenseResult.ERROR
+    if n <= 9:
+        return DefenseResult.NOTHING
+    elif n <= 11:
+        return DefenseResult.REDUCE_HIT
+    else:
+        return DefenseResult.OUT
 
 
 def hit():
     n = randint(1, 20)
+    if n <= 2:
+        return SwingResult(SwingResultType.HIT, (1, 1))
+    elif n <= 7:
+        defense_result = defense()
+        if defense_result is DefenseResult.NOTHING or defense_result is DefenseResult.REDUCE_HIT:
+            return SwingResult(SwingResultType.HIT, (1, 1))
+        elif defense_result is DefenseResult.ERROR:
+            return SwingResult(SwingResultType.HIT, (2, 2))
+        else:
+            return SwingResult(SwingResultType.OUT, 1)
+    elif n <= 12:
+        return SwingResult(SwingResultType.HIT, (1, 2))
+    elif n <= 15:
+        defense_result = defense()
+        if defense_result is DefenseResult.NOTHING:
+            return SwingResult(SwingResultType.HIT, (2, 2))
+        elif defense_result is DefenseResult.ERROR:
+            return SwingResult(SwingResultType.HIT, (3, 3))
+        elif defense_result is DefenseResult.REDUCE_HIT:
+            return SwingResult(SwingResultType.HIT, (1, 2))
+        else:
+            return SwingResult(SwingResultType.OUT, 1)
+    elif n <= 17:
+        return SwingResult(SwingResultType.HIT, (2, 3))
+    elif n == 18:
+        defense_result = defense()
+        if defense_result is DefenseResult.NOTHING:
+            return SwingResult(SwingResultType.HIT, (3, 3))
+        elif defense_result is DefenseResult.ERROR:
+            return SwingResult(SwingResultType.HIT, (4, 4))
+        elif defense_result is DefenseResult.REDUCE_HIT:
+            return SwingResult(SwingResultType.HIT, (2, 3))
+        else:
+            return SwingResult(SwingResultType.OUT, 1)
+    else:
+        return SwingResult(SwingResultType.HIT, (4, 4))
 
 
 def crit_hit():
     n = randint(1, 20)
+    if n <= 12:
+        return SwingResult(SwingResultType.HIT, (2, 3))
+    elif n <= 17:
+        return SwingResult(SwingResultType.HIT, (3, 4))
+    else:
+        return SwingResult(SwingResultType.HIT, (4, 4))
