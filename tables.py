bbc2d352b56346268c668cd92df8b6e5695bf4c7
Jonathan Moyett <someguy590@gmail.com>
2020-10-07 20:47:36 -0500

    fill outs

--- tables.py
+++ tables.py
@@ -3,6 +3,22 @@ from random import randint
 
 def out():
     n = randint(0, 9)
+    if n <= 2:
+        return 'K'
+    elif n == 3:
+        return 'G-3'
+    elif n == 4:
+        return '4-3'
+    elif n == 5:
+        return '5-3'
+    elif n == 6:
+        return '6-3'
+    elif n == 7:
+        return 'F-7'
+    elif n == 8:
+        return 'F-8'
+    else:
+        return 'F-9'
 
 
 def defense():
